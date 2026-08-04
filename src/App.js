41860ab1817edeb6e73c30c8c71799beb11e73ea
Tariq Horan <tariqhoran88@gmail.com>
2026-08-04 13:15:56 +0100

    unused /

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Filecomp from "./pages/Filecomp";
-import Emails from "./pages/Email";
-import Prompts from "./pages/Prompts";
+// import Home from "./pages/Home";
+// import Filecomp from "./pages/Filecomp";
+// import Emails from "./pages/Email";
+// import Prompts from "./pages/Prompts";
 import Data from "./pages/Data";
-import Comparer from "./pages/CredentialComparison";
+// import Comparer from "./pages/CredentialComparison";
 import Canada from "./pages/Canada";
 
 const App = () => {
