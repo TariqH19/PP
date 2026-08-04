7fb8196623a848eab86b3ba68114a7cc216abc1d
Tariq Horan <tariqhoran88@gmail.com>
2026-08-04 13:18:30 +0100

    unused var

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <>
       {/* <Nav /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/noscope" element={<Comparer />} /> */}
         <Route path="/data" element={<Data />} />
         {/* <Route path="/prompts" element={<Prompts />} /> */}
